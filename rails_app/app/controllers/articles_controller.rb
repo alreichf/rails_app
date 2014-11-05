70ac9d66733f1074b269ccc30e29e9738e1d5a58
alreichf <alreich.fernandes@gmail.com>
2014-11-05 22:51:12 +0000

    - some more changes - for CRUD

--- rails_app/app/controllers/articles_controller.rb
+++ rails_app/app/controllers/articles_controller.rb
@@ -1,15 +1,18 @@
 class ArticlesController < ApplicationController
 
     def new
-        
+       @article = Article.new 
     end
 
     def create
         #render plain: params[:article].inspect
         @article = Article.new(article_params)
 
-        @article.save
-        redirect_to @article 
+        if @article.save
+            redirect_to @article 
+        else
+            render "new"
+        end
     end
 
     def show
@@ -18,6 +21,27 @@ class ArticlesController < ApplicationController
 
     def index
         @articles = Article.all
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render 'edit' 
+        end
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+
+        redirect_to articles_path
     end
 
     private
